5f219d9599861c7ed2d635353e1a0d554bc46e97
yeoning <sy03549@gmail.com>
2024-01-01 21:50:29 +0900

    feat: webpack 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
 const nextConfig = {
-  output: 'export',
+  output: 'standalone',
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, '../../')
+  },
   reactStrictMode: true,
   images: {
     unoptimized: true
+  },
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
   }
 };
 
