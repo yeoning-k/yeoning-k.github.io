303bec1a8434750fd9135ccbeb0ad998ce708424
yeoning <sy03549@gmail.com>
2024-01-01 17:27:54 +0900

    feat: container font 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,16 @@ import '@/styles/reset.css';
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import Header from '@/components/Header';
+import { Noto_Sans_KR } from 'next/font/google';
+const NotoSans_KR = Noto_Sans_KR({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
-      <Component {...pageProps} />
+      <div id="container" className={NotoSans_KR.className}>
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
